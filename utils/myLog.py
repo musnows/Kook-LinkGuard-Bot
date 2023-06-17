10cd1f267815e30de7d898f89334f585b3b7f7b9
musnow <ezplayingd@126.com>
2023-06-17 17:27:17 +0800

    feat(myLog): using RotatingFileHandler for log rotate

--- utils/myLog.py
+++ utils/myLog.py
@@ -1,11 +1,14 @@
 import sys
 import logging # 采用logging来替换所有print
+from logging.handlers import RotatingFileHandler
 from datetime import datetime,timezone,timedelta
 from khl import Message,PrivateMessage
 
 LOGGER_NAME = "bot-log" # 日志对象名字，这个没啥用
 LOGGER_FILE = "bot.log" # 如果想修改log文件的名字和路径，修改此变量
 """日志文件路径"""
+LOGGER_FILE_BYTES = 10*1024*1024
+"""日志文件达到多少B时分割,10*1024*1024是10mb"""
 
 #将获取当前时间封装成函数方便使用
 def get_time(format_str='%y-%m-%d %H:%M:%S'):
@@ -53,7 +56,8 @@ _log = logging.getLogger(LOGGER_NAME)
 """自定义的logger对象"""
 # 实例化控制台handler和文件handler，同时输出到控制台和文件
 # cmd_handler = logging.StreamHandler() # 默认设置里面，就会往控制台打印信息;自己又加一个，导致打印俩次
-file_handler = logging.FileHandler(LOGGER_FILE, mode="a", encoding="utf-8")
+# 使用RotatingFileHandler，每10mb一个日志文件
+file_handler = RotatingFileHandler(filename=LOGGER_FILE, mode='a',maxBytes=LOGGER_FILE_BYTES,encoding='utf-8', backupCount=7)
 fmt = logging.Formatter(fmt="[%(asctime)s] %(levelname)s:%(filename)s:%(funcName)s:%(lineno)d | %(message)s",
                     datefmt="%y-%m-%d %H:%M:%S")
 file_handler.setFormatter(fmt)
